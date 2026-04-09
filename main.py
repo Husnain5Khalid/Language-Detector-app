e4e4e7aeaf92851255dd719f11d8d818f5096b0a
Husnain Khalid <139623958+Husnain5Khalid@users.noreply.github.com>
2026-04-09 18:12:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import predict_pipeline
+
+
+app = FastAPI()
+
+class TextIn(BaseModel):
+    text: str
+
+class PredictionOut(BaseModel):
+    language: str
+
+@app.get("/")
+def home():
+    return {"health_check": "OK"}
+
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict_pipeline(payload.text)
+    return {"language": language}
+
